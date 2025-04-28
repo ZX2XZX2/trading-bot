0d6c7d9a1b42217feb0c648aca1d5562442dffdc
ZX2XZX2 <zx2xzx27979@gmail.com>
2025-04-28 17:47:41 -0400

    Switch to live feed

--- trading_bot/market_data.py
+++ trading_bot/market_data.py
@@ -1,7 +1,7 @@
 from ib_insync import IB, Ticker
 
 def request_market_data(ib: IB, contract) -> Ticker:
-    ib.reqMarketDataType(3)  # 1=live, 3=delayed if necessary
+    ib.reqMarketDataType(1)  # 1=live, 3=delayed if necessary
     ticker = ib.reqMktData(contract)
     ib.sleep(2)
     return ticker
